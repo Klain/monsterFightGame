a89bd6545664e959600fd228e0c2d84739656b53
Klain <klain.lafuente@gmail.com>
2025-02-24 11:53:37 +0100

    DASDASD

---
+++ backend/src/notes/notes.ts
@@ -0,0 +1,90 @@
+/*
+REVISION:
+    FRONT:
+        PERFIL:
+            LISTO:
+                MOSTRAR DATOS PRINCIPALES
+                MOSTRAR ATRIBUTOS
+                MOSTRAR UPGRADE
+                UPGRADE ATRIBUTOS
+                MOSTRAR EQUIPO
+                MOSTRAR INVENTARIO
+            PENDIENTE:
+                MOSTRAR ESTADISTICAS??
+                MOSTRAR PARAMETROS EXTRA
+                REVISAR EQUIPAR - DESEQUIPAR.
+                MOSTRAR ATRIBUTOS COMBINADOS.
+                AÑADIR TALENTOS??
+                AÑADIR ORBES ("Modo historia mod parametros segun respuestas")
+
+        ACTIVIDADES:
+            LISTO:
+                MOSTRAR LISTADO DE ACTIVIDADES
+                MOSTRAR DURACION MAXIMA
+                INICIAR ACTIVIDAD
+                CONTROLAR NO INICIAR SI HAY ACTIVIDAD
+                CONTROLAR DURACION MAXIMA
+                COMPLETAR ACTIVIDAD
+                    RECOMPENSA ACTIVIDAD
+                
+            REVISAR:
+                CONTROLAR NO INICIAR ESTADO CHARACTER
+
+        COMBATES:
+            LISTO:
+                MOSTRAR LISTADO DE OPONENTES
+                CONTROLAR ESTADO CHARACTER
+                INICIAR COMBATE
+                MOSTRAR RESULTADO COMBATE
+                ACTUALIZAR RECOMPENSAS
+            PENDIENTE:
+                MOSTRAR RANKING
+                FILTROS RANKING??
+                AMISTAD??
+
+        TIENDA:
+            LISTO:
+                MOSTRAR OBJETOS TIENDA
+                PERMITIR COMPRAR
+
+            PENDIENTE:    
+                CORREGIR MOSTRAR INVENTARIO.
+                REVISAR COMPRA - VENTA.
+                    REVISAR APILAMIENTO OBJETOS
+                REVISAR ACTUALIZACION WEBSOCKET.
+
+        MENSAJES:
+            LISTO:
+                MOSTRAR MENSAJES ENTRADA
+                MOSTRAR MENSAJES SALIDA
+                ENVIAR NUEVO MENSAJE
+
+            PENDIENTE:
+                REVSAR DUPLICADO ENTRANTES-SALIENTES
+                REVISAR ELIMINACION.
+        
+        ATRIBUTOS:
+            REVISAR ATRIBUTOS COMBINADOS PERFIL Y COMBATE
+
+        TODO:
+            AÑADIR AMISTAD
+            AÑADIR GUARIDA
+            AÑADIR EQUIPOS ( ARMADURAS ACCESORIOS)
+            AÑADIR CONSUMIBLES
+            AÑADIR CREACION PERSONAJE ( NOMBRE - CLASE)
+            AÑADIR GREMIOS??
+            AÑADIR ESTADOS?? 
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
