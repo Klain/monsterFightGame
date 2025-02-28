14a9dc82e4f950600ea58e735316da99ed73f19e
Klain <klain.lafuente@gmail.com>
2025-02-28 14:21:15 +0100

    Actualizacion notas

--- backend/src/notes/notes.ts
+++ backend/src/notes/notes.ts
@@ -5,93 +5,97 @@
       VALIDACIONES USERNAME
     CREACION DE PERSONAJE
       AÑADIR ELECCION NOMBRE Y CLASE
+    RANKING
+      MOSTRAR RANKING
+        AÑADIR SOLICITUD AMISTAD
+    MENSAJES
+      DESTINATARIO
+        MOSTRAR AMIGOS
+    AMISTADES
+      MOSTAR AMISTADES
+        ELIMINAR AMISTAD
+      MOSTRAR SOLICITUDES
+        ACEPTAR SOLICITUD
+        RECHAZAR SOLICITUD
+
+
+    AÑADIR GUARIDA
+      cofre : minimo de oro que no te podran robar nunca
+      almacen: aumenta los espacios del inventario
+      entorno : reduce la posibilidad de recibir un ataque ( a la hora de que un 
+      oponente busque enemigos tienes una posibilidad de no aparecer en su listado)
+      trampas/alarmas :  reduce la posibilidad de recibir un robo.
+      (a futuro añadire mesas de crafteo o cosas asi pero por ahora no)
+      aguas termales?? : regeneracion de salud
+      piedra de mana?? : regeneracion de mana ( no en este juego al menos...)
     
+    AÑADIR EQUIPOS ( ARMADURAS ACCESORIOS)
+    AÑADIR CONSUMIBLES
+    AÑADIR CREACION PERSONAJE ( NOMBRE - CLASE)
+    AÑADIR GREMIOS??
+    AÑADIR ESTADOS?? 
 */
 /*
-REVISION:
-    FRONT:
-        PERFIL:
-            LISTO:
-                MOSTRAR DATOS PRINCIPALES
-                MOSTRAR ATRIBUTOS
-                MOSTRAR UPGRADE
-                UPGRADE ATRIBUTOS
-                MOSTRAR EQUIPO
-                MOSTRAR INVENTARIO
-            PENDIENTE:
-                MOSTRAR ESTADISTICAS??
-                MOSTRAR PARAMETROS EXTRA
-                REVISAR EQUIPAR - DESEQUIPAR.
-                MOSTRAR ATRIBUTOS COMBINADOS.
-                AÑADIR TALENTOS??
-                AÑADIR ORBES ("Modo historia mod parametros segun respuestas")
+  PERFIL:
+    LISTO:
+        MOSTRAR DATOS PRINCIPALES
+        MOSTRAR ATRIBUTOS
+        MOSTRAR UPGRADE
+        UPGRADE ATRIBUTOS
+        MOSTRAR EQUIPO
+        MOSTRAR INVENTARIO
+    PENDIENTE:
+        MOSTRAR ESTADISTICAS??
+        MOSTRAR PARAMETROS EXTRA
+        REVISAR EQUIPAR - DESEQUIPAR.
+        MOSTRAR ATRIBUTOS COMBINADOS.
+        AÑADIR TALENTOS??
+        AÑADIR ORBES ("Modo historia mod parametros segun respuestas")
+*/
+/*
+  ACTIVIDADES:
+    LISTO:
+        MOSTRAR LISTADO DE ACTIVIDADES
+        MOSTRAR DURACION MAXIMA
+        INICIAR ACTIVIDAD
+        CONTROLAR NO INICIAR SI HAY ACTIVIDAD
+        CONTROLAR DURACION MAXIMA
+        COMPLETAR ACTIVIDAD
+            RECOMPENSA ACTIVIDAD
+    REVISAR:
+        CONTROLAR NO INICIAR ESTADO CHARACTER
+*/
+/*
+  COMBATES:
+    LISTO:
+        MOSTRAR LISTADO DE OPONENTES
+        CONTROLAR ESTADO CHARACTER
+        INICIAR COMBATE
+        MOSTRAR RESULTADO COMBATE
+        ACTUALIZAR RECOMPENSAS
+    REVISAR:
+        MOSTRAR RANKING
+    PENDIENTE:
+        FILTROS RANKING??
+        AMISTAD??
+*/
+/*
+  TIENDA:
+    LISTO:
+        MOSTRAR OBJETOS TIENDA
+        PERMITIR COMPRAR
 
-        ACTIVIDADES:
-            LISTO:
-                MOSTRAR LISTADO DE ACTIVIDADES
-                MOSTRAR DURACION MAXIMA
-                INICIAR ACTIVIDAD
-                CONTROLAR NO INICIAR SI HAY ACTIVIDAD
-                CONTROLAR DURACION MAXIMA
-                COMPLETAR ACTIVIDAD
-                    RECOMPENSA ACTIVIDAD
-            REVISAR:
-                CONTROLAR NO INICIAR ESTADO CHARACTER
+    PENDIENTE:    
+      REVISAR APILAMIENTO OBJETOS
+*/
+/*
+  MENSAJES:
+    LISTO:
+        MOSTRAR MENSAJES ENTRADA
+        MOSTRAR MENSAJES SALIDA
+        ENVIAR NUEVO MENSAJE
 
-        COMBATES:
-            LISTO:
-                MOSTRAR LISTADO DE OPONENTES
-                CONTROLAR ESTADO CHARACTER
-                INICIAR COMBATE
-                MOSTRAR RESULTADO COMBATE
-                ACTUALIZAR RECOMPENSAS
-            REVISAR:
-                MOSTRAR RANKING
-            PENDIENTE:
-                FILTROS RANKING??
-                AMISTAD??
-
-        TIENDA:
-            LISTO:
-                MOSTRAR OBJETOS TIENDA
-                PERMITIR COMPRAR
-
-            PENDIENTE:    
-                CORREGIR MOSTRAR INVENTARIO.
-                REVISAR COMPRA - VENTA.
-                    REVISAR APILAMIENTO OBJETOS
-                REVISAR ACTUALIZACION WEBSOCKET.
-
-        MENSAJES:
-            LISTO:
-                MOSTRAR MENSAJES ENTRADA
-                MOSTRAR MENSAJES SALIDA
-                ENVIAR NUEVO MENSAJE
-
-            PENDIENTE:
-                REVSAR DUPLICADO ENTRANTES-SALIENTES
-                REVISAR ELIMINACION.
-        
-        ATRIBUTOS:
-            REVISAR ATRIBUTOS COMBINADOS PERFIL Y COMBATE
-
-        TODO:
-            
-            AÑADIR GUARIDA
-              cofre : minimo de oro que no te podran robar nunca
-              almacen: aumenta los espacios del inventario
-              entorno : reduce la posibilidad de recibir un ataque ( a la hora de que un 
-              oponente busque enemigos tienes una posibilidad de no aparecer en su listado)
-              trampas/alarmas :  reduce la posibilidad de recibir un robo.
-              (a futuro añadire mesas de crafteo o cosas asi pero por ahora no)
-              aguas termales?? : regeneracion de salud
-              piedra de mana?? : regeneracion de mana ( no en este juego al menos...)
-            AÑADIR EQUIPOS ( ARMADURAS ACCESORIOS)
-            AÑADIR CONSUMIBLES
-            AÑADIR CREACION PERSONAJE ( NOMBRE - CLASE)
-            AÑADIR GREMIOS??
-            AÑADIR ESTADOS?? 
-
-        REVISAR: 
-            AÑADIR AMISTAD
+    PENDIENTE:
+        REVSAR DUPLICADO ENTRANTES-SALIENTES
+        REVISAR ELIMINACION.
 */
