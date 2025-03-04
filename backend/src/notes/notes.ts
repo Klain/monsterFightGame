8701472f47507029b629d6ffaf1453684ed72408
Klain <klain.lafuente@gmail.com>
2025-03-04 17:17:51 +0100

    Actualizacion notas

--- backend/src/notes/notes.ts
+++ backend/src/notes/notes.ts
@@ -1,7 +1,8 @@
 
 /*
   TODO
-    AÑADIR GUARIDA
+    Validar GUARIDA 
+      El componente esta añadido pero no se tienen en cuenta los valores.
       cofre : minimo de oro que no te podran robar nunca
       almacen: aumenta los espacios del inventario
       entorno : reduce la posibilidad de recibir un ataque ( a la hora de que un 
@@ -10,8 +11,12 @@
       (a futuro añadire mesas de crafteo o cosas asi pero por ahora no)
       aguas termales?? : regeneracion de salud
       piedra de mana?? : regeneracion de mana ( no en este juego al menos...)
+
+    Validar attributes 
+      el componente esta añadido pero no se tienen en cuenta todo creo
     
-    AÑADIR EQUIPOS ( ACCESORIOS)
+    Validar accesorios ( si tienen efectos royo mejora de robo y asi)
+
     AÑADIR CONSUMIBLES
 
     AÑADIR GREMIOS??
@@ -20,77 +25,45 @@
 */
 /*
   REGISTRO:
-        BACK:
-        FRONT:
-          MEJORAR TODO
+    BACK:
+    FRONT:
   CREACION DE PERSONAJE:
-        BACK:
-          CREACION DE PERSONAJE
-        FRONT:
-          LOGICA : Si un user hace login y no tiene personaje, un guard le redirecciona a creacion de personaje
-          GUARD NOT_CHARACTER : Si user no tiene character redir createCharacterComponent
-          GUARD HAS_CHARACTER : Aplicado solo para createCharacterComponent. Si user tiene character redir dashboard
-          CREACION DE PERSONAJE COMPONENT : Rellenar nombre y clase, crear personaje y el guard lo llevara a dashboard, creo...
+    BACK:
+    FRONT:
   PERFIL:
-    LISTO:
-        MOSTRAR DATOS PRINCIPALES
-        MOSTRAR ATRIBUTOS
-        MOSTRAR UPGRADE
-        UPGRADE ATRIBUTOS
-        MOSTRAR EQUIPO
-        MOSTRAR INVENTARIO
+    BACK:
+    FRONT:
     PENDIENTE:
-        MOSTRAR ESTADISTICAS??
-        MOSTRAR PARAMETROS EXTRA
-        REVISAR EQUIPAR - DESEQUIPAR.
-        MOSTRAR ATRIBUTOS COMBINADOS.
-        AÑADIR TALENTOS??
-        AÑADIR ORBES ("Modo historia mod parametros segun respuestas")
+      MOSTRAR ESTADISTICAS??
+      MOSTRAR PARAMETROS EXTRA
+      MOSTRAR ATRIBUTOS COMBINADOS.
+      AÑADIR TALENTOS??
+      AÑADIR ORBES ("Modo historia mod parametros segun respuestas")
 */
 /*
   ACTIVIDADES:
-    LISTO:
-        MOSTRAR LISTADO DE ACTIVIDADES
-        MOSTRAR DURACION MAXIMA
-        INICIAR ACTIVIDAD
-        CONTROLAR NO INICIAR SI HAY ACTIVIDAD
-        CONTROLAR DURACION MAXIMA
-        COMPLETAR ACTIVIDAD
-            RECOMPENSA ACTIVIDAD
-    REVISAR:
-        CONTROLAR NO INICIAR ESTADO CHARACTER
+    BACK:
+    FRONT:
+    PENDIENTE:
+      añadir un control para que durante la actividad no se puedan realizar otras acciones (front y back)
 */
 /*
   COMBATES:
-    LISTO:
-        MOSTRAR LISTADO DE OPONENTES
-        CONTROLAR ESTADO CHARACTER
-        INICIAR COMBATE
-        MOSTRAR RESULTADO COMBATE
-        ACTUALIZAR RECOMPENSAS
-    REVISAR:
-        MOSTRAR RANKING
+    BACK:
+    FRONT:
     PENDIENTE:
         FILTROS RANKING??
-        AMISTAD??
 */
 /*
   TIENDA:
-    LISTO:
-        MOSTRAR OBJETOS TIENDA
-        PERMITIR COMPRAR
-
-    PENDIENTE:    
-      REVISAR APILAMIENTO OBJETOS
+    BACK:
+    FRONT:
+    PENDIENTE:
 */
 /*
   MENSAJES:
-    LISTO:
-        MOSTRAR MENSAJES ENTRADA
-        MOSTRAR MENSAJES SALIDA
-        ENVIAR NUEVO MENSAJE
-
+    BACK:
+    FRONT:
     PENDIENTE:
-        REVSAR DUPLICADO ENTRANTES-SALIENTES
-        REVISAR ELIMINACION.
+        REVISAR ELIMINACION (solo se guarda el mensaje tanto para entrantes como salientes...)
 */
